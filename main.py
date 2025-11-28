7b98d58a750e1f5c99fe5708942d963797879449
stephanie hartono <mail4hartono@gmail.com>
2025-11-29 00:12:33 +1100

    added main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+from fastapi import FastAPI
+# from routes.tasks import router as tasks_router #make directory to link this to later
+
+# app = FastAPI()
+# app.include_router(tasks_router, prefix="/api/tasks")
